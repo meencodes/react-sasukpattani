0fd27382c7f92f17e3a4dc3f9fa9ebd83f639f63
majasmee <ameen_aic@hotmail.com>
2019-08-01 20:56:23 +0700

    Login api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class App extends Component {
 
-  submitlogin(e) {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: ""
+    }
+
+    // this.login = this.login.bind(this);
+    // this.change = this.change.bind(this);
 
   }
+
+  submitlogin(e) {
+
+    console.log('good')
+    const { username, password } = this.state
+    const data = { username: username, password: password }
+
+    Axios.post('http://localhost:8080/api/v1/login', data)
+    .then(res => {
+      const result = res.data
+
+      if (result.result === "success") {
+        // localStorage.setItem("token", result.data)
+        console.log('success')
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  change(e) {
+    this.setState({[e.target.name]: e.target.value});
+    console.log(this.state);
+  } 
 
   render() {
     return (
@@ -12,11 +46,11 @@ class App extends Component {
         <table>
           <tr>
             <td>username</td>
-            <td><input type="text" name="username" /></td>
+            <td><input type="text" name="username" onChange={this.change.bind(this)}/></td>
           </tr>
           <tr>
             <td>password</td>
-            <td><input type="password" name="password" /></td>
+            <td><input type="password" name="password" onChange={this.change.bind(this)} /></td>
           </tr>
           <tr align='center'>
             <td colSpan="2">
